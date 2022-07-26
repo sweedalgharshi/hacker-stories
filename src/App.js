20ea76e731fd6e3a24d6ff03f4364175fddfe243
Sweedal Gharshi <sweedalgracius@gmail.com>
2022-07-26 13:22:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+  //you can do something in between
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark ',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  // A : a callback function gets introduced
+  const handleSearch = (event) => {
+    //C : but, "calls back" to the place it was called
+    console.log(event.target.value);
+  };
+
+  // console.log('App Renders');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My Hacker Stories</h1>
+
+      {/* B*/}
+      <Search onSearch={handleSearch} />
+      <hr />
+      <List list={stories} />
+      {/* render the list here */}
     </div>
   );
-}
+};
 
+const List = ({ list }) => {
+  // console.log('List Renders');
+  return (
+    <ul>
+      {list.map((item) => (
+        <Item item={item} key={item.objectID} />
+      ))}
+    </ul>
+  );
+};
+
+const Item = ({ item }) => {
+  // console.log('Item renders');
+  return (
+    <li>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+    </li>
+  );
+};
+
+const Search = ({ onSearch }) => {
+  //do  something in between
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+    onSearch(event);
+  };
+
+  // console.log('Search');
+  return (
+    <div>
+      <label htmlFor="search">Search:</label>
+      <input type="text" id="search" onChange={handleChange} />
+      <p>
+        Searching for <strong>{searchTerm}</strong>
+      </p>
+    </div>
+  );
+};
 export default App;
