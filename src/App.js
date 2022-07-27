ff8258cfedd50937c7c77dda71ee163d7977c7e6
Sweedal Gharshi <sweedalgracius@gmail.com>
2022-07-27 12:08:52 +0530

    Learned more about hooks i.e useEffect and done some refactoring

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const App = () => {
   //you can do something in between
@@ -22,11 +22,24 @@ const App = () => {
     },
   ];
 
-  // A : a callback function gets introduced
+  const [searchTerm, setSearchTerm] = useState(
+    localStorage.getItem('search') || 'React'
+  );
+
+  useEffect(() => {
+    localStorage.setItem('search', searchTerm);
+  }, [searchTerm]);
+
+  // A : a callback function gets sintroduced
   const handleSearch = (event) => {
     //C : but, "calls back" to the place it was called
-    console.log(event.target.value);
+    console.log(searchTerm);
+    setSearchTerm(event.target.value);
   };
+
+  const searchedStories = stories.filter((story) =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   // console.log('App Renders');
   return (
@@ -34,9 +47,9 @@ const App = () => {
       <h1>My Hacker Stories</h1>
 
       {/* B*/}
-      <Search onSearch={handleSearch} />
+      <Search onSearch={handleSearch} searchTerm={searchTerm} />
       <hr />
-      <List list={stories} />
+      <List list={searchedStories} />
       {/* render the list here */}
     </div>
   );
@@ -47,7 +60,7 @@ const List = ({ list }) => {
   return (
     <ul>
       {list.map((item) => (
-        <Item item={item} key={item.objectID} />
+        <Item key={item.objectID} item={item} />
       ))}
     </ul>
   );
@@ -67,23 +80,20 @@ const Item = ({ item }) => {
   );
 };
 
-const Search = ({ onSearch }) => {
+const Search = ({ onSearch, searchTerm }) => {
   //do  something in between
-  const [searchTerm, setSearchTerm] = useState('');
 
-  const handleChange = (event) => {
-    setSearchTerm(event.target.value);
-    onSearch(event);
-  };
+  // const handleChange = (event) => {
+  //   setSearchTerm(event.target.value);
 
-  // console.log('Search');
+  //   //B: is used elsewhere
+  //   onSearch(event);
+  // };
   return (
     <div>
-      <label htmlFor="search">Search:</label>
-      <input type="text" id="search" onChange={handleChange} />
-      <p>
-        Searching for <strong>{searchTerm}</strong>
-      </p>
+      <label htmlFor="search">Search: </label>
+      <input type="text" id="search" onChange={onSearch} value={searchTerm} />
+      <p>Searching for: {searchTerm}</p>
     </div>
   );
 };
