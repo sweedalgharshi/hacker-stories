76cc98e74a1bb37a1980e08c7bea74432c2884ac
Sweedal Gharshi <sweedalgracius@gmail.com>
2022-07-28 12:26:18 +0530

    Learned about Conditional rendering of JSX based on state or props made two useState based on it isLoading and isError and the JSX will load based on this true or false value and use ternary operator and logical operator respectively

--- src/App.js
+++ src/App.js
@@ -43,13 +43,18 @@ const App = () => {
     );
 
   const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
-
   const [stories, setStories] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
-    getAsyncStories().then((result) => {
-      setStories(result.data.stories);
-    });
+    setIsLoading(true);
+    getAsyncStories()
+      .then((result) => {
+        setStories(result.data.stories);
+        setIsLoading(false);
+      })
+      .catch(() => setIsError(true));
   }, []);
 
   const handleRemoveStory = (item) => {
@@ -87,7 +92,15 @@ const App = () => {
         <strong>Search :</strong>
       </InputWithLabel>
       <hr />
-      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+
+      {isError && <p>Something went wrong....</p>}
+
+      {isLoading ? (
+        <p>Loading.....</p>
+      ) : (
+        <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+      )}
+
       {/* render the list here */}
     </div>
   );
